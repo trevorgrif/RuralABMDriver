8c28790f6836ec093042e9a0a1b7338a9031c26d
trevorgrif <trevorg0715@gmail.com>
2023-07-07 15:46:24 -0700

    Fix run.jl

--- run.jl
+++ run.jl
@@ -4,11 +4,12 @@ using Distributed
 @everywhere Pkg.instantiate()
 @everywhere using RuralABMDriver
 
-RuralABMDriver.Run_RuralABM(
+RuralABMDriver.run_ruralABM(
    SOCIAL_NETWORKS = 10,
    NETWORK_LENGTH = 30,
    MASKING_LEVELS = 5,
    VACCINATION_LEVELS = 5,
    DISTRIBUTION_TYPE = [0, 0], #Order is [MASK, VAX], 0 = Random, 1 = Watts
-   MODEL_RUNS = 100
+   MODEL_RUNS = 100,
+   TOWN_NAMES = ["small"],
    )
